35473d3af545da6f0e1f003ad687b0660a2c7f2d
thesevenlayers <rafael@thesevenlayers.net>
2014-05-21 18:28:29 +0200

    added twig widget extension

---
+++ src/Zgh/FEBundle/TwigExtension/WidgetsExtension.php
@@ -0,0 +1,33 @@
+<?php
+namespace Zgh\FEBundle\TwigExtension;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Zgh\FEBundle\Entity\User;
+
+class WidgetsExtension extends \Twig_Extension
+{
+    protected $em;
+
+    public function __construct(EntityManagerInterface $entityManagerInterface)
+    {
+        $this->em = $entityManagerInterface;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction("getExpriences", [$this, "getExpriences"])
+        ];
+    }
+
+    public function getExpriences(User $user)
+    {
+        $experiences = $this->em->getRepository("ZghFEBundle:Experience")->findByUser($user);
+        return $experiences;
+    }
+
+    public function getName()
+    {
+        return "zgh_fe_widgets";
+    }
+}
