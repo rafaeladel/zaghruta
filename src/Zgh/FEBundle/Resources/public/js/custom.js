948027a6150139b8b6f66bf64b207d6c4f286b1b
Rafael Adel <rafae@thesevnelayers.net>
2014-08-03 16:50:00 +0300

    bugs

--- src/Zgh/FEBundle/Resources/public/js/custom.js
+++ src/Zgh/FEBundle/Resources/public/js/custom.js
@@ -87,15 +87,15 @@ $(window).resize(function () {
 $(".contentInterests ul").addClass("col-md-12");
 $(".contentInterests ul li").addClass("col-md-3");
 
-    $(document).ready(function () {
-             $(".iconGroup a").mouseenter(function(){
-            var secName = $(this).attr('class');
-            $('#' + secName + '').siblings().hide();
-            $('#' + secName + '').fadeIn(100);
-        });
-        $(".iconGroup a").mouseleave(function(){
-          $(".active").show();
-          $(".hide-show").hide();
-        });
-          });
+$(document).ready(function () {
+    $(".iconGroup a").mouseenter(function () {
+        var secName = $(this).attr('class');
+        $('#' + secName + '').siblings().hide();
+        $('#' + secName + '').fadeIn(100);
+    });
+    $(".iconGroup a").mouseleave(function () {
+        $(".active").show();
+        $(".hide-show").hide();
+    });
+});
 
