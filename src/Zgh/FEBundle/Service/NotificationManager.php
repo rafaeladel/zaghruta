04ac2ca1e807a3c45d2584e2b23acb93389f62dd
rafael <rafael.adel20@gmail.com>
2014-10-27 16:00:08 +0200

    raf

--- src/Zgh/FEBundle/Service/NotificationManager.php
+++ src/Zgh/FEBundle/Service/NotificationManager.php
@@ -1,11 +1,20 @@
 <?php
 namespace Zgh\FEBundle\Service;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Zgh\FEBundle\Entity\Notification;
 use Zgh\FEBundle\Entity\User;
 
 class NotificationManager
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em;
+    }
     public function create(User $user, array $content, $action_id, $type)
     {
         $notification = new Notification();
@@ -13,6 +22,8 @@ class NotificationManager
         $notification->setUser($user);
         $notification->setActionId($action_id);
         $notification->setType($type);
+        $this->em->persist($notification);
+        $this->em->flush();
         return $notification;
     }
 }
