51f280fdd3077da64e153572f027b52a32737b45
rafael <rafael.adel20@gmail.com>
2014-11-19 11:38:19 +0200

    raf

--- src/Zgh/FEBundle/EventListener/LoginSuccessHandler.php
+++ src/Zgh/FEBundle/EventListener/LoginSuccessHandler.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
@@ -13,10 +14,12 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerI
 class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
 {
     private $router;
+    private $session;
 
-    public function __construct(UrlGeneratorInterface $router)
+    public function __construct(UrlGeneratorInterface $router, Session $session)
     {
         $this->router = $router;
+        $this->session = $session;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token)
@@ -32,6 +35,9 @@ class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
             return new RedirectResponse($this->router->generate("zgh_fe.user_profile.user_intro_edit", array(
                     "id" => $user->getId()
                 )));
+        }
+        if($this->session->has("_security.main.target_path")) {
+            return new RedirectResponse($this->session->get("_security.main.target_path"));
         }
         return new RedirectResponse($this->router->generate("zgh_fe.wall.index"));
     }
