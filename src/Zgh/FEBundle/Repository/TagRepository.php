327254be0949bfe535c31c4a32430b6122a5ff23
Rafael Adel <rafae@thesevnelayers.net>
2015-01-01 18:06:24 +0200

    raf

--- src/Zgh/FEBundle/Repository/TagRepository.php
+++ src/Zgh/FEBundle/Repository/TagRepository.php
@@ -7,33 +7,33 @@ use Doctrine\ORM\Query\Expr\Join;
 
 class TagRepository extends EntityRepository
 {
-    public function findSearchResult($criteria, $user)
-    {
-        $q = $this->createQueryBuilder("t")
-                ->select("t")
-                ->innerJoin("t.users", "u", Join::WITH, "u.id = :user")
-                ->where("t.name LIKE :name")
-                ->setParameter("user", $user)
-                ->setParameter("name", "%".$criteria."%")
-                ->orderBy("t.name", "ASC")
-                ->getQuery();
-        return $q->getResult();
-    }
-
-//
 //    public function findSearchResult($criteria, $user)
 //    {
 //        $q = $this->createQueryBuilder("t")
-//            ->select("t")
-//            ->innerJoin("t.products", "p")
-//            ->innerJoin("p.user", "u", Expr\Join::WITH, "u.id = :user")
-//            ->where("t.name LIKE :name")
-//            ->andWhere("u.id = :user")
-//            ->setParameters([
-//                "name" => "%".$criteria."%",
-//                "user" => $user
-//            ]);
-//
-//        return $q->getQuery()->getResult();
+//                ->select("t")
+//                ->innerJoin("t.users", "u", Join::WITH, "u.id = :user")
+//                ->where("t.name LIKE :name")
+//                ->setParameter("user", $user)
+//                ->setParameter("name", "%".$criteria."%")
+//                ->orderBy("t.name", "ASC")
+//                ->getQuery();
+//        return $q->getResult();
 //    }
+
+
+    public function findSearchResult($criteria, $user)
+    {
+        $q = $this->createQueryBuilder("t")
+            ->select("t")
+            ->innerJoin("t.products", "p")
+            ->innerJoin("p.user", "u", Expr\Join::WITH, "u.id = :user")
+            ->where("t.name LIKE :name")
+            ->andWhere("u.id = :user")
+            ->setParameters([
+                "name" => "%".$criteria."%",
+                "user" => $user
+            ]);
+
+        return $q->getQuery()->getResult();
+    }
 }
