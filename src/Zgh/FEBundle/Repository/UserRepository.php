e3d238d0bc25ba815b71118a00da9455b3f28ff1
unknown <Ahmed_elashqar@RIS-NB-3095.HeadQ.raya.corp>
2015-01-08 02:11:52 +0200

    ahmed

--- src/Zgh/FEBundle/Repository/UserRepository.php
+++ src/Zgh/FEBundle/Repository/UserRepository.php
@@ -72,7 +72,7 @@ class UserRepository extends EntityRepository
     public function getPublicPosts($user, $offset = null, $id_holder = null)
     {
         $rsm = new ResultSetMapping;
-        $rsm->addEntityResult('Zgh\FEBundle\Entity\Post', 'post');
+        $rsm->addEntityResult('Post', 'post');
         $rsm->addFieldResult('post', 'id', 'id');
         $rsm->addFieldResult('post', 'user_id', 'user_id');
         $rsm->addFieldResult('post', 'fullname', 'fullname');
